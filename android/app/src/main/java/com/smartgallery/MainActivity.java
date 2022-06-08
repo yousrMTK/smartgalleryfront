b488b703f534bbedf13320c583c265894a3aa42b
ahmedsam <ahmedsamran1412@gmail.com>
2022-06-08 19:22:45 +0900

    Convert to mobile app

---
+++ android/app/src/main/java/com/smartgallery/MainActivity.java
@@ -0,0 +1,5 @@
+package com.smartgallery;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smartgallery',
+  appName: 'Smart Gallery',
+  webDir: 'dist/smartgalleryfront',
+  bundledWebRuntime: false
+};
+
+export default config;
